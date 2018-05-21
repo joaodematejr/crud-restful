94fb52b34a96e00ca8246e151ef52726820f7e55
João Dematé <joaodematejr@gmail.com>
2018-05-21 11:37:09 -0300

    Atualização Adição/ Edição e Buscar Corrigido erro das depedencias

---
+++ crud-restful/src/br/com/joao/dao/NotaDAO.java
@@ -0,0 +1,81 @@
+package br.com.joao.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.joao.config.BDConfig;
+import br.com.joao.entity.Nota;
+
+public class NotaDAO {
+
+	// SALVAR
+	public void addNota(Nota nota) throws Exception {
+		int idGerado = 0;
+		Connection conexao = BDConfig.getConnection();
+
+		String sql = "INSERT INTO TB_NOTA(TITULO, DESCRICAO) VALUES(?, ?)";
+
+		PreparedStatement statement = conexao.prepareStatement(sql);
+		statement.setString(1, nota.getTitulo());
+		statement.setString(2, nota.getDescricao());
+		statement.execute();
+	}
+
+	// LISTAR
+	public List<Nota> listarNotas() throws Exception {
+		List<Nota> lista = new ArrayList<>();
+		Connection conexao = BDConfig.getConnection();
+		String sql = "SELECT * FROM TB_NOTA";
+		PreparedStatement statement = conexao.prepareStatement(sql);
+		ResultSet rs = statement.executeQuery();
+		while (rs.next()) {
+			Nota nota = new Nota();
+			nota.setId(rs.getInt("ID"));
+			nota.setTitulo(rs.getString("TITULO"));
+			nota.setDescricao(rs.getString("DESCRICAO"));
+			lista.add(nota);
+		}
+		return lista;
+	}
+
+	// LISTAR POR ID
+	public Nota buscarNotaPorId(int id) throws Exception {
+		Nota nota = null;
+		Connection conexao = BDConfig.getConnection();
+		String sql = "SELECT * FROM TB_NOTA WHERE ID = ?";
+		PreparedStatement statement = conexao.prepareStatement(sql);
+		statement.setInt(1, id);
+		ResultSet rs = statement.executeQuery();
+		if (rs.next()) {
+			nota = new Nota();
+			nota.setId(rs.getInt("ID"));
+			nota.setTitulo(rs.getString("TITULO"));
+			nota.setDescricao(rs.getString("DESCRICAO"));
+		}
+		return nota;
+	}
+
+	// EDITAR
+	public void editarNota(Nota nota, int id) throws Exception {
+		Connection conexao = BDConfig.getConnection();
+		String sql = "UPDATE TB_NOTA SET TITULO = ?, DESCRICAO = ? WHERE ID = ?";
+		PreparedStatement statement = conexao.prepareStatement(sql);
+		statement.setString(1, nota.getTitulo());
+		statement.setString(2, nota.getDescricao());
+		statement.setInt(3, id);
+		statement.execute();
+	}
+
+	// DELETAR
+	public void removerNota(int idNota) throws Exception {
+		Connection conexao = BDConfig.getConnection();
+		String sql = "DELETE FROM TB_NOTA WHERE ID = ?";
+		PreparedStatement statement = conexao.prepareStatement(sql);
+		statement.setInt(1, idNota);
+		statement.execute();
+	}
+
+}
---
+++ crud-restful/src/br/com/joao/rest/NotasService.java
@@ -0,0 +1,79 @@
+package br.com.joao.rest;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.com.joao.dao.NotaDAO;
+import br.com.joao.entity.Nota;
+
+@Path("/notas")
+public class NotasService {
+
+	private static final String CHARSET_UTF8 = ";charset=utf-8";
+
+	private NotaDAO notaDAO;
+
+	@PostConstruct
+	private void init() {
+		notaDAO = new NotaDAO();
+	}
+
+	// ADICIONAR
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_JSON + CHARSET_UTF8)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String addNota(Nota nota) {
+		String msg = "";
+		System.out.println(nota.getTitulo());
+		try {
+			notaDAO.addNota(nota);
+
+			msg = "Nota add com sucesso!";
+		} catch (Exception e) {
+			msg = "Erro ao add a nota!";
+			e.printStackTrace();
+		}
+
+		return msg;
+	}
+
+	// LISTAR
+	@GET
+	@Path("/list")
+	@Produces(MediaType.APPLICATION_JSON + CHARSET_UTF8)
+	public List<Nota> listarNotas() {
+		List<Nota> lista = null;
+		try {
+			lista = notaDAO.listarNotas();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return lista;
+	}
+
+	// BUSCAR POR ID
+	@GET
+	@Path("/get/{id}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON + CHARSET_UTF8)
+	public Nota buscarPorId(@PathParam("id") int id) {
+		Nota nota = null;
+		try {
+			nota = notaDAO.buscarNotaPorId(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return nota;
+	}
+
+}
